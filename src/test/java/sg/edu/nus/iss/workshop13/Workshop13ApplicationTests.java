57f0479e04cc9e8839562fe944e26ea476bf13c6
Keefe Tan Kui Yu <keefe@Keefes-MacBook-Pro.local>
2022-03-03 09:34:19 +0800

    test case

--- src/test/java/sg/edu/nus/iss/workshop13/Workshop13ApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/workshop13/Workshop13ApplicationTests.java
@@ -1,13 +1,22 @@
 package sg.edu.nus.iss.workshop13;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import sg.edu.nus.iss.workshop13.controller.AddressBookController;
 
 @SpringBootTest
 class Workshop13ApplicationTests {
 
+	@Autowired
+	private AddressBookController controller;
+
 	@Test
 	void contextLoads() {
+			assertThat(controller).isNotNull();
 	}
 
 }
